45ff09f3dd69847e350dc4feda12705d80f95a90
Andreas Caspers <casperli@noreply.users.github.com>
2017-05-09 15:14:46 +0200

    New header as web-component embedded

--- src/src/main.ts
+++ src/src/main.ts
@@ -2,7 +2,6 @@
 // we want font-awesome to load as soon as possible to show the fa-spinner
 import '../static/styles.css';
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import { Aurelia } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
 import * as Bluebird from 'bluebird';
