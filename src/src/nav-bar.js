a36e14486af9527d4ceef3dea937ac84a3a28f0c
Andreas Caspers <casperli@noreply.users.github.com>
2017-05-10 18:51:16 +0200

    New version

---
+++ src/src/nav-bar.js
@@ -0,0 +1,10 @@
+// (function () {
+//   if ('registerElement' in document && 'import' in document.createElement('link') && 'content' in document.createElement('template')) {
+//     // platform is good! 
+//   } else {
+//     // polyfill the platform! 
+//     var e = document.createElement('script'); e.src = '/node_modules/webcomponentsjs/webcomponents-lite.min.js';
+//     document.body.appendChild(e);
+//   }
+// })();
+
